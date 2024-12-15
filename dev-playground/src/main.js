b1f090e5a07c39b028443777e7f60666a8eca455
Artem Nistuley <artem.nistuley@gmail.com>
2024-12-15 16:41:13 +0200

    add vite config and example page

--- dev-playground/src/main.js
+++ dev-playground/src/main.js
@@ -1,7 +1,6 @@
 import './assets/styles/main.css';
 import '@kameo/core/style/prosemirror.css';
 import '@kameo/core/style/main.css';
-
 import { initKameo } from './scripts/index.js';
 
 initKameo({
---
+++ dev-playground/vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          example: resolve(__dirname, 'examples/example.html'),
+        },
+      },
+    },
+  }
+});
