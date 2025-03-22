ecec995a9203d72d4bec73a41c998608d74de901
Artem Nistuley <artem.nistuley@gmail.com>
2025-03-22 14:42:54 +0200

    minor changes

--- dev-playground/src/helpers/createBaseForm.js
+++ dev-playground/src/helpers/createBaseForm.js
@@ -2,7 +2,7 @@ export const createBaseForm = (kameo) => {
   const pos = kameo.state.doc.content.size;
   kameo
     .chain()
-    .insertContentAt(pos, '<h1>Simple form</h1>')
+    .insertContentAt(pos, '<h1>Simple form</h1><p>This is a <b>simple</b> form</p>')
     .insertFormInputName(pos)
     .insertFormInputEmail(pos)
     .insertFormInputText(pos)
