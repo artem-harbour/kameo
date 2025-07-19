ced825b6fc33f50c62f230e5104a7fbf5f2b4277
Artem Nistuley <artem.nistuley@gmail.com>
2025-07-19 18:10:59 +0300

    create FormElement class, add exports

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -3,7 +3,10 @@ export * from '@tiptap/core';
 
 // Kameo core
 export * from './Kameo.js';
+export * from './FormManager.js';
+export * from './FormElement.js';
 export * from './FormElementView.js';
+export * from './FormElementStore.js';
 export * as kameoHelpers from './helpers/index.js';
 export * as kameoUtils from './utilities/index.js';
 export * as kameoExtensions from './extensions/index.js';
